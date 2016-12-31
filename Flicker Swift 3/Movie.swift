f743e587ed21a29d6c8e2a5a89b2b62f0107f282
Siraj Zaneer <szaneer@gmail.com>
2016-12-30 18:23:02 -0600

    Initial

---
+++ Flicker Swift 3/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  Flicker Swift 3
+//
+//  Created by Siraj Zaneer on 12/30/16.
+//  Copyright © 2016 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+
+class Movie: NSObject {
+
+}
---
+++ Pods/Target Support Files/Pods-Flicker Swift 3/Pods-Flicker Swift 3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flicker_Swift_3 : NSObject
+@end
+@implementation PodsDummy_Pods_Flicker_Swift_3
+@end
